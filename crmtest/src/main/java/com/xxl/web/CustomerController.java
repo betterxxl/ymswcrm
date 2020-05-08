af388919e59fc7d65840f2217c95ed96577138e8
rain <745310219@qq.com>
2020-05-08 09:58:08 +0800

    rain提交

---
+++ crmtest/src/main/java/com/xxl/web/CustomerController.java
@@ -0,0 +1,19 @@
+package com.xxl.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author better
+ * @Date 2020/4/23 18:46
+ * @Version 666
+ */
+@Controller
+@RequestMapping("customer")
+public class CustomerController {
+    @GetMapping("login")
+    public  String login(){
+        return "customer";
+    }
+}
---
+++ crmtest/src/main/webapp/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mloong
+  Date: 2020/4/23
+  Time: 18:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>登录....</h1>
+</body>
+</html>
